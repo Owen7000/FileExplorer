19e558f5fc8710bec49c9e3b886ff3959182b3e0
Owen <77976026+Owen7000@users.noreply.github.com>
2025-02-03 11:44:14 +0000

    Add project files.

---
+++ FileExplorer/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pnlTopPanel = New System.Windows.Forms.Panel()
+        Me.scMainAppContent = New System.Windows.Forms.SplitContainer()
+        Me.gbSelectedFile = New System.Windows.Forms.GroupBox()
+        Me.rtbSelectedFile = New System.Windows.Forms.RichTextBox()
+        Me.tvDirectoryInfo = New System.Windows.Forms.TreeView()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        CType(Me.scMainAppContent, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.scMainAppContent.Panel1.SuspendLayout()
+        Me.scMainAppContent.Panel2.SuspendLayout()
+        Me.scMainAppContent.SuspendLayout()
+        Me.gbSelectedFile.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlTopPanel
+        '
+        Me.pnlTopPanel.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlTopPanel.Location = New System.Drawing.Point(0, 0)
+        Me.pnlTopPanel.Name = "pnlTopPanel"
+        Me.pnlTopPanel.Size = New System.Drawing.Size(800, 94)
+        Me.pnlTopPanel.TabIndex = 0
+        '
+        'scMainAppContent
+        '
+        Me.scMainAppContent.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.scMainAppContent.Location = New System.Drawing.Point(0, 94)
+        Me.scMainAppContent.Name = "scMainAppContent"
+        '
+        'scMainAppContent.Panel1
+        '
+        Me.scMainAppContent.Panel1.Controls.Add(Me.gbSelectedFile)
+        '
+        'scMainAppContent.Panel2
+        '
+        Me.scMainAppContent.Panel2.Controls.Add(Me.tvDirectoryInfo)
+        Me.scMainAppContent.Size = New System.Drawing.Size(800, 481)
+        Me.scMainAppContent.SplitterDistance = 266
+        Me.scMainAppContent.TabIndex = 1
+        '
+        'gbSelectedFile
+        '
+        Me.gbSelectedFile.Controls.Add(Me.rtbSelectedFile)
+        Me.gbSelectedFile.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gbSelectedFile.Location = New System.Drawing.Point(0, 0)
+        Me.gbSelectedFile.Name = "gbSelectedFile"
+        Me.gbSelectedFile.Size = New System.Drawing.Size(266, 481)
+        Me.gbSelectedFile.TabIndex = 0
+        Me.gbSelectedFile.TabStop = False
+        Me.gbSelectedFile.Text = "Selected File"
+        '
+        'rtbSelectedFile
+        '
+        Me.rtbSelectedFile.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtbSelectedFile.Enabled = False
+        Me.rtbSelectedFile.Location = New System.Drawing.Point(3, 22)
+        Me.rtbSelectedFile.Name = "rtbSelectedFile"
+        Me.rtbSelectedFile.ReadOnly = True
+        Me.rtbSelectedFile.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.rtbSelectedFile.Size = New System.Drawing.Size(260, 456)
+        Me.rtbSelectedFile.TabIndex = 0
+        Me.rtbSelectedFile.Text = ""
+        '
+        'tvDirectoryInfo
+        '
+        Me.tvDirectoryInfo.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tvDirectoryInfo.ImageIndex = 2
+        Me.tvDirectoryInfo.ImageList = Me.ImageList1
+        Me.tvDirectoryInfo.Location = New System.Drawing.Point(0, 0)
+        Me.tvDirectoryInfo.Name = "tvDirectoryInfo"
+        Me.tvDirectoryInfo.SelectedImageIndex = 1
+        Me.tvDirectoryInfo.ShowNodeToolTips = True
+        Me.tvDirectoryInfo.ShowPlusMinus = False
+        Me.tvDirectoryInfo.Size = New System.Drawing.Size(530, 375)
+        Me.tvDirectoryInfo.StateImageList = Me.ImageList1
+        Me.tvDirectoryInfo.TabIndex = 0
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "css.png")
+        Me.ImageList1.Images.SetKeyName(1, "fo.png")
+        Me.ImageList1.Images.SetKeyName(2, "f.png")
+        Me.ImageList1.Images.SetKeyName(3, "default.png")
+        Me.ImageList1.Images.SetKeyName(4, "cog.png")
+        Me.ImageList1.Images.SetKeyName(5, "f-user.png")
+        Me.ImageList1.Images.SetKeyName(6, "fo-user.png")
+        Me.ImageList1.Images.SetKeyName(7, "text.png")
+        Me.ImageList1.Images.SetKeyName(8, "fo-cloud.png")
+        Me.ImageList1.Images.SetKeyName(9, "f-cloud.png")
+        Me.ImageList1.Images.SetKeyName(10, "git.png")
+        Me.ImageList1.Images.SetKeyName(11, "gitignore.png")
+        Me.ImageList1.Images.SetKeyName(12, "exe.png")
+        Me.ImageList1.Images.SetKeyName(13, "fo-vsc.png")
+        Me.ImageList1.Images.SetKeyName(14, "f-vsc.png")
+        Me.ImageList1.Images.SetKeyName(15, "f-config.png")
+        Me.ImageList1.Images.SetKeyName(16, "fo-config.png")
+        Me.ImageList1.Images.SetKeyName(17, "fo-app.png")
+        Me.ImageList1.Images.SetKeyName(18, "f-app.png")
+        Me.ImageList1.Images.SetKeyName(19, "fo-docs.png")
+        Me.ImageList1.Images.SetKeyName(20, "f-docs.png")
+        Me.ImageList1.Images.SetKeyName(21, "py.png")
+        Me.ImageList1.Images.SetKeyName(22, "html.png")
+        Me.ImageList1.Images.SetKeyName(23, "yaml.png")
+        Me.ImageList1.Images.SetKeyName(24, "pdf.png")
+        Me.ImageList1.Images.SetKeyName(25, "php.png")
+        Me.ImageList1.Images.SetKeyName(26, "md.png")
+        Me.ImageList1.Images.SetKeyName(27, "vid.png")
+        Me.ImageList1.Images.SetKeyName(28, "audio.png")
+        Me.ImageList1.Images.SetKeyName(29, "img.png")
+        Me.ImageList1.Images.SetKeyName(30, "shell.png")
+        Me.ImageList1.Images.SetKeyName(31, "ps1.png")
+        Me.ImageList1.Images.SetKeyName(32, "data.png")
+        Me.ImageList1.Images.SetKeyName(33, "fo-code.png")
+        Me.ImageList1.Images.SetKeyName(34, "f-code.png")
+        Me.ImageList1.Images.SetKeyName(35, "f-test.png")
+        Me.ImageList1.Images.SetKeyName(36, "fo-test.png")
+        Me.ImageList1.Images.SetKeyName(37, "f-py.png")
+        Me.ImageList1.Images.SetKeyName(38, "fo-py.png")
+        Me.ImageList1.Images.SetKeyName(39, "py2.png")
+        Me.ImageList1.Images.SetKeyName(40, "java.png")
+        '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 475)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 100)
+        Me.Panel1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 575)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.scMainAppContent)
+        Me.Controls.Add(Me.pnlTopPanel)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.scMainAppContent.Panel1.ResumeLayout(False)
+        Me.scMainAppContent.Panel2.ResumeLayout(False)
+        CType(Me.scMainAppContent, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.scMainAppContent.ResumeLayout(False)
+        Me.gbSelectedFile.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlTopPanel As Panel
+    Friend WithEvents scMainAppContent As SplitContainer
+    Friend WithEvents tvDirectoryInfo As TreeView
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents gbSelectedFile As GroupBox
+    Friend WithEvents rtbSelectedFile As RichTextBox
+End Class
---
+++ FileExplorer/Form1.vb
@@ -0,0 +1,183 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim rootNode As New TreeNode("c:/")
+        addDirectoryInfoToTreeView("c:/", rootNode)
+    End Sub
+
+
+
+    Private Sub tvDirectoryInfo_NodeMouseClick(sender As Object, e As TreeNodeMouseClickEventArgs) Handles tvDirectoryInfo.NodeMouseClick
+        If e.Button = MouseButtons.Left Then
+            If e.Node.IsExpanded Then
+                e.Node.Collapse()
+                e.Node.Nodes.Clear()
+            Else
+                e.Node.Expand()
+                If e.Node.Tag = "Directory" Then
+                    Try
+                        addDirectoryInfoToTreeView(e.Node.FullPath.Replace("\", "/"), e.Node)
+                    Catch ex As System.UnauthorizedAccessException
+                        MessageBox.Show("Access to that directory is restricted and it cannot be opened.")
+                    End Try
+                End If
+            End If
+
+            If e.Node.Text.EndsWith(".txt") Then
+                rtbSelectedFile.Text = readTextFile("c:/" + e.Node.Text)
+            End If
+        End If
+    End Sub
+
+    Private Sub tvDirectoryInfo_NodeMouseDoubleClick(sender As Object, e As TreeNodeMouseClickEventArgs) Handles tvDirectoryInfo.NodeMouseDoubleClick
+        If e.Button = MouseButtons.Left Then
+            If e.Node.Tag = "File" Then
+                OpenFile("c:/" + e.Node.Text)
+            End If
+        End If
+    End Sub
+
+    Public Function readTextFile(path As String)
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText(path)
+
+        Return fileReader
+    End Function
+
+    Private Sub OpenFile(ByVal File As String)
+        Try
+            Dim myProcess As New Process
+            myProcess.StartInfo.FileName = File
+            myProcess.StartInfo.UseShellExecute = True
+            myProcess.StartInfo.RedirectStandardOutput = False
+            myProcess.Start()
+            myProcess.Dispose()
+        Catch ex As System.ComponentModel.Win32Exception
+            MessageBox.Show("There is no application available to open this type of file.")
+        End Try
+    End Sub
+
+    Private Sub addDirectoryInfoToTreeView(path As String, rootNode As TreeNode)
+        rootNode.Nodes.Clear()
+
+        Dim FileLocation As DirectoryInfo = New DirectoryInfo(path)
+
+        Dim subDirectories = FileLocation.GetDirectories()
+        Dim dri As DirectoryInfo
+
+        For Each dri In subDirectories
+            Dim childNode As New TreeNode(dri.Name)
+            childNode.Tag = "Directory"
+
+            If dri.Name.StartsWith("User") Then
+                childNode.ImageIndex = 5
+                childNode.SelectedImageIndex = 6
+            ElseIf dri.Name.Contains("OneDrive") Then
+                childNode.ImageIndex = 9
+                childNode.SelectedImageIndex = 8
+            ElseIf dri.Name = ".vscode" Then
+                childNode.ImageIndex = 14
+                childNode.SelectedImageIndex = 13
+            ElseIf dri.Name = ".config" Then
+                childNode.ImageIndex = 15
+                childNode.SelectedImageIndex = 16
+            ElseIf dri.Name.StartsWith("App") Then
+                childNode.ImageIndex = 18
+                childNode.SelectedImageIndex = 17
+            ElseIf dri.Name.StartsWith("Doc") Or dri.Name.StartsWith("doc") Then
+                childNode.ImageIndex = 19
+                childNode.SelectedImageIndex = 20
+            ElseIf dri.Name.Contains("src") Or dri.Name.Contains("source") Or dri.Name.Contains("code") Then
+                childNode.ImageIndex = 34
+                childNode.SelectedImageIndex = 33
+            ElseIf dri.Name = "__pycache__" Or dri.Name = ".venv" Then
+                childNode.ImageIndex = 37
+                childNode.SelectedImageIndex = 38
+            Else
+                childNode.ImageIndex = 2
+            End If
+
+            rootNode.Nodes.Add(childNode)
+        Next
+
+        ' Get a reference to each file in that directory.
+        Dim fiArr As FileInfo() = FileLocation.GetFiles()
+        ' Display the names of the files.
+        Dim fri As FileInfo
+        For Each fri In fiArr
+            Debug.WriteLine(fri.Extension)
+            Dim childNode As New TreeNode(fri.Name)
+            childNode.Tag = "File"
+
+            If fri.Extension = ".dll" Or fri.Extension = ".ini" Or fri.Extension = ".cfg" Then
+                childNode.ImageIndex = 4
+                childNode.SelectedImageIndex = 4
+            ElseIf fri.Extension = ".txt" Or fri.Extension = ".tmp" Or fri.Extension = ".log" Then
+                childNode.ImageIndex = 7
+                childNode.SelectedImageIndex = 7
+            ElseIf fri.Extension.StartsWith(".git") Then
+                childNode.ImageIndex = 10
+                childNode.SelectedImageIndex = 10
+            ElseIf fri.Extension = ".gitignore" Then
+                childNode.ImageIndex = 11
+                childNode.SelectedImageIndex = 11
+            ElseIf fri.Extension = ".exe" Then
+                childNode.ImageIndex = 12
+                childNode.SelectedImageIndex = 12
+            ElseIf fri.Extension = ".py" Then
+                childNode.ImageIndex = 21
+                childNode.SelectedImageIndex = 21
+            ElseIf fri.Extension = ".pyc" Then
+                childNode.ImageIndex = 39
+                childNode.SelectedImageIndex = 39
+            ElseIf fri.Extension = ".html" Then
+                childNode.ImageIndex = 22
+                childNode.SelectedImageIndex = 22
+            ElseIf fri.Extension = ".css" Then
+                childNode.ImageIndex = 0
+                childNode.SelectedImageIndex = 0
+            ElseIf fri.Extension = ".php" Then
+                childNode.ImageIndex = 25
+                childNode.SelectedImageIndex = 25
+            ElseIf fri.Extension = ".md" Then
+                childNode.ImageIndex = 26
+                childNode.SelectedImageIndex = 26
+            ElseIf fri.Extension = ".pdf" Then
+                childNode.ImageIndex = 24
+                childNode.SelectedImageIndex = 24
+            ElseIf fri.Extension = ".yml" Then
+                childNode.ImageIndex = 23
+                childNode.SelectedImageIndex = 23
+            ElseIf fri.Extension = ".mp3" Then
+                childNode.ImageIndex = 28
+                childNode.SelectedImageIndex = 28
+            ElseIf fri.Extension = ".png" Or fri.Extension = ".jpg" Or fri.Extension = ".jpeg" Or fri.Extension = ".gif" Then
+                childNode.ImageIndex = 29
+                childNode.SelectedImageIndex = 29
+            ElseIf fri.Extension = ".bash" Or fri.Extension = ".cmd" Or fri.Extension = ".bat" Then
+                childNode.ImageIndex = 30
+                childNode.SelectedImageIndex = 30
+            ElseIf fri.Extension = ".ps1" Then
+                childNode.ImageIndex = 31
+                childNode.SelectedImageIndex = 31
+            ElseIf fri.Extension = ".sql" Or fri.Extension = ".db" Or fri.Extension = ".sqlite" Then
+                childNode.ImageIndex = 32
+                childNode.SelectedImageIndex = 32
+            ElseIf fri.Extension = ".java" Or fri.Extension = ".jar" Then
+                childNode.ImageIndex = 40
+                childNode.SelectedImageIndex = 40
+            Else
+                childNode.ImageIndex = 3
+                childNode.SelectedImageIndex = 3
+            End If
+
+            rootNode.Nodes.Add(childNode)
+        Next fri
+
+        Try
+            tvDirectoryInfo.Nodes.Add(rootNode)
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
